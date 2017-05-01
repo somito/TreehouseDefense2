2d11c7e333f26cdf2151be2ce7351f562fafa032
Soma <Soma@SOMA-PC>
2017-05-01 14:42:38 +0200

    added test for in range of method in maplocation class

--- TreehouseDefenseTests/MapLocationTests.cs
+++ TreehouseDefenseTests/MapLocationTests.cs
@@ -16,5 +16,13 @@ namespace TreehouseDefense.Tests
             var map = new Map(3, 3);
             var exception = Assert.Throws<OutOfBoundsException>(() => new MapLocation(3, 3, map));
         }
+
+        [Fact()]
+        public void InRangeOfWithRange1()
+        {
+            var map = new Map(3, 3);
+            var target = new MapLocation(0, 0, map);
+            Assert.True(target.InRangeOf(new MapLocation(0, 1, map), 1));
+        }
     }
 }
