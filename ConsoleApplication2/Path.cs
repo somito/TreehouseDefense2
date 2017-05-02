5c0c70359bce510b44a16125e92a60df7a4d7f77
somito <somodian@hotmail.com>
2017-05-02 15:28:14 +0200

    added pathtests isonpath and isnotonpath

--- ConsoleApplication2/Path.cs
+++ ConsoleApplication2/Path.cs
@@ -1,6 +1,6 @@
 ﻿namespace TreehouseDefense
 {
-    class Path
+    public class Path
     {
         private readonly MapLocation[] _path;
 
---
+++ TreehouseDefenseTests/PathTests.cs
@@ -0,0 +1,47 @@
+﻿using Xunit;
+
+namespace TreehouseDefense.Tests
+{
+
+
+    
+    public class PathTests
+    {
+        private Map _map3x3;
+        private MapLocation[] _pathLocations3;
+        private Path _path3;
+
+        public PathTests()
+        {
+            _map3x3 = new Map(3, 3);
+
+            _pathLocations3 = new MapLocation[]
+                {
+                    new MapLocation(0, 1, _map3x3),
+                    new MapLocation(1, 1, _map3x3),
+                    new MapLocation(2, 1, _map3x3)
+                };
+
+            _path3 = new Path(_pathLocations3);
+        }
+
+
+        [Fact]
+        public void MapLocationIsOnPath()
+        {
+            var target = _path3;
+
+            Assert.True(target.OnPath(new MapLocation(0, 1, _map3x3)));
+        }
+
+        [Fact]
+        public void MapLocationIsNotOnPath()
+        {
+            var target = _path3;
+
+            Assert.False(target.OnPath(new MapLocation(0, 0, _map3x3)));
+        }
+
+
+    }
+}
